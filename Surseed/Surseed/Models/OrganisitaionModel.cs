3e2725db4bac6beef5a88bdfb91b865093abf126
DEV-DOTNET-SUDHANSU <DEV-DOTNET-SUDHANSU@WINDOWS-112>
2019-08-21 16:34:42 +0530

    new model created

---
+++ Surseed/Surseed/Models/OrganisitaionModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Surseed.Models
+{
+    public class OrganisitaionModel
+    {
+        public class Resistraion
+        {
+            public int OrganizationId { get; set; }
+            public string OrganizationName { get; set; }
+            public string ContactNumber { get; set; }
+            public string Address { get; set; }
+
+            public string City { get; set; }
+            public string State { get; set; }
+
+            public string Zip { get; set; }
+
+            public string EIN { get; set; }
+
+            public string TAXID { get; set; }
+
+            public Boolean OrganizationActive { get; set; }
+
+            public Int64 OrganizationUserId { get; set; }
+
+            public string OrganizationUserTyepId { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+
+            public string Gender { get; set; }
+
+            public string Age { get; set; }
+
+            public string Phone { get; set; }
+
+            public string EmailId { get; set; }
+
+            public string Password { get; set; }
+
+            public string ConfPassword { get; set; }
+            public Boolean OrganizationUserActive { get; set; }
+        }
+
+        public class Login
+        {
+            public string EmailId { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
